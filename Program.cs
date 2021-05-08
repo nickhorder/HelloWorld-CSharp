ff738002d142f2c2a0d3373305302e729354bbfd
nickhorder <thunderstruck1986@hotmail.co.uk>
2021-05-08 12:23:43 +0100

    Pushing first C# program made in Visual Studio - HelloWorld

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+/* Nick's first C# program
+   Here we are writing the famous and beloved "Hello World" program in C#. 
+*/
+//
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Lots of love, Nick Horder");
+        }
+    }
+}
